beacc2673e6e8f7db996b79e1a8b7d3f054b6999
swapnendu-karmakar <ktskarmakar@gmail.com>
2022-06-20 21:46:59 +0530

    initial commit

---
+++ necessary-pkg.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "###########################################"
+echo "###### installing necessary packages ######"
+echo "###########################################"
+
+sudo pacman -S --needed git curl wget base-devel vlc firefox htop neofetch gparted vim neovim pcmanfm
